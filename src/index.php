ba0aa4785023d15d8175c4592c6a5f5df45e0ead
alenka <fdsfs>
2019-09-09 11:16:58 +0300

    вывести ошибки

--- src/index.php
+++ src/index.php
@@ -6,7 +6,8 @@ $errors = [
     'email' => array(),
     'password' => array(),
     'passwordConfirmation' => array(),
-    'birthday' => array(),
+    'birthdate' => array(),
+    'sex' => array(),
     'score' => array(),
     'website' => array(),
 ];
@@ -21,26 +22,26 @@ if ($_POST) {
     $password = $_POST['password'];
     if (!not_empty_validator($password))
         $errors['password'][] = 'Password required';
-    elseif (string_length_validator($password) < 8)
-        $errors['password'][] = 'Password should contain more than 8 symbols';
+    elseif (!string_length_validator($password, 8, 20))
+        $errors['password'][] = 'Password should contain more than 8 and less than 20 symbols';
 
     $passwordConfirmation = $_POST['passwordConfirmation'];
     if (!not_empty_validator($passwordConfirmation))
         $errors['passwordConfirmation'][] = 'Password confirmation required';
-    elseif (string_length_validator($passwordConfirmation) < 8)
-        $errors['passwordConfirmation'][] = 'Password should contain more than 8 symbols';
+    elseif (!string_length_validator($passwordConfirmation, 8, 20))
+        $errors['passwordConfirmation'][] = 'Password should contain more than 8 and less than 20 symbols';
     elseif (!identical_validator($password, $passwordConfirmation))
         $errors['passwordConfirmation'][] = 'Passwords dont match';
 
     $sex = $_POST['sex'];
-    if (!in_array($sex, [0, 1], 1))
+    if (!in_array($sex, ['0', '1'], 1))
         $errors['sex'][] = 'Magic happened';
 
     $birthdate = $_POST['birthdate'];
     if (!not_empty_validator($birthdate))
-        $errors['birthday'][] = 'Birthday required';
+        $errors['birthdate'][] = 'Birthday required';
     elseif (!date_validator($birthdate))
-        $errors['birthday'][] = 'When you was born?';
+        $errors['birthdate'][] = 'When you was born?';
 
     $score = $_POST['score'];
     if (!digit_validator($score))
@@ -53,6 +54,15 @@ if ($_POST) {
         $errors['website'][] = 'Website required';
     elseif (!uri_validator($website))
         $errors['website'][] = 'Website should be a valid URL address';
+
+    $count = null;
+    foreach ($errors as $error) {
+        $count += count($error);
+    }
+
+    if (!$count) {
+        echo "<div class=\"alert alert-success\">" . 'Registration successful' . "</div>";
+    }
 }
 
 ?>
@@ -77,13 +87,34 @@ if ($_POST) {
         <p class="h4 mb-4">Register</p>
 
         <!-- Email -->
-        <input type="text" name="email" class="form-control mb-4" placeholder="E-mail">
+        <input type="text" name="email" class="form-control mb-4" placeholder="E-mail" value=<?= $_POST['email'] ?? ""?> >
+        <?php if (count($errors['email'])): ?>
+            <div>
+                <?php foreach ($errors['email'] as $error): ?>
+                    <p class="alert alert-primary"> <?= $error; ?> </p>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
 
         <!-- Password -->
         <input type="password" name="password" class="form-control mb-4" placeholder="Password">
+        <?php if (count($errors['password'])): ?>
+            <div>
+                <?php foreach ($errors['password'] as $error): ?>
+                    <p class="alert alert-primary"> <?= $error; ?> </p>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
 
         <!-- Password confirmation -->
         <input type="password" name="passwordConfirmation" class="form-control mb-4" placeholder="Password confirmation">
+        <?php if (count($errors['passwordConfirmation'])): ?>
+            <div>
+                <?php foreach ($errors['passwordConfirmation'] as $error): ?>
+                    <p class="alert alert-primary"> <?= $error; ?> </p>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
 
         <!-- Sex -->
         <div class="form-group form-control mb-4">
@@ -95,17 +126,45 @@ if ($_POST) {
                 <input class="form-check-input" type="radio" name="sex" id="female" value="1">
                 <label class="form-check-label" for="female">Female</label>
             </div>
+            <?php if (count($errors['sex'])): ?>
+                <div>
+                    <?php foreach ($errors['sex'] as $error): ?>
+                        <p class="alert alert-primary"> <?= $error; ?> </p>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
         </div>
 
         <!-- Birthdate -->
-        <input type="text" name="birthdate" class="form-control mb-4" placeholder="Birthdate">
+        <input type="text" name="birthdate" class="form-control mb-4" placeholder="Birthdate" value=<?= $_POST['birthdate'] ?? ""?> >
+        <?php if (count($errors['birthdate'])): ?>
+            <div>
+                <?php foreach ($errors['birthdate'] as $error): ?>
+                    <p class="alert alert-primary"> <?= $error; ?> </p>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
 
         <!-- Score -->
 
-        <input type="text" name="score" class="form-control mb-4" placeholder="Score">
+        <input type="text" name="score" class="form-control mb-4" placeholder="Score" value=<?= $_POST['score'] ?? ""?> >
+        <?php if (count($errors['score'])): ?>
+            <div>
+                <?php foreach ($errors['score'] as $error): ?>
+                    <p class="alert alert-primary"> <?= $error; ?> </p>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
 
         <!-- Website -->
-        <input type="text" name="website" class="form-control mb-4" placeholder="Website">
+        <input type="text" name="website" class="form-control mb-4" placeholder="Website" value=<?= $_POST['website'] ?? ""?> >
+        <?php if (count($errors['website'])): ?>
+            <div>
+                <?php foreach ($errors['website'] as $error): ?>
+                    <p class="alert alert-primary"> <?= $error; ?> </p>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
 
 
         <!-- Register button -->
